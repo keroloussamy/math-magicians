c8445ec986459dac6cd0def3e9d0a3b3726d3b49
kerolous samy <keroloussamy98@gmail.com>
2022-01-11 23:19:47 +0200

    Add some style

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,8 +8,8 @@ function Navbar() {
     <nav>
       <h1 className="nav-header">Math Magicians</h1>
       <ul>
-        <Link to="/math-magicians/" style={{ textDecoration: 'none', marginRight: 10 }}>Home</Link>
-        <Link to="/math-magicians/calculator" style={{ textDecoration: 'none', marginRight: 10 }}>Calculator</Link>
+        <Link to="/math-magicians/" style={{ textDecoration: 'none', marginRight: 10 }}>Home |</Link>
+        <Link to="/math-magicians/calculator" style={{ textDecoration: 'none', marginRight: 10 }}>Calculator |</Link>
         <Link to="/math-magicians/quote" style={{ textDecoration: 'none', marginRight: 10 }}>Quote</Link>
       </ul>
     </nav>
